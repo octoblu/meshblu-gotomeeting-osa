e5f8217b563102b2161018800ced248b6291452e
Peter DeMartini and Roy van de Water <dev+peter+roy@integrumtech.com>
2014-12-12 13:07:20 -0700

    Move start gotomeeting logic to external file

--- index.js
+++ index.js
@@ -3,65 +3,7 @@ var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 var osa = require('osa');
 var debug = require('debug')('meshblu-gotomeeting-osa:index');
-
-var GoToMeeting = (function() {
-  var GoToMeetingStarter = (function() {
-    var self = this;
-
-    self.go = (function() {
-      var GoToMeetingApp = Application('/Applications/GoToMeeting.app');
-      GoToMeetingApp.activate();
-
-      var SystemEvents = Application('System Events');
-      self.GoToMeeting = SystemEvents.processes['GoToMeeting'];
-
-      self.waitForFirstWindow();
-      GoToMeeting.windows[0].groups[3].buttons[0].click();
-
-      self.waitForSecondWindow();
-      self.waitForRecordButton();
-      self.startRecording();
-    });
-
-    self.startRecording = (function() {
-      console.log('startRecording');
-      var index = self.GoToMeeting.windows[1].groups.length - 1;
-      GoToMeeting.windows[1].groups[index].checkboxes[0].click();
-    });
-
-    self.waitForRecordButton = (function() {
-      console.log('waitForRecordButton');
-      var index = self.GoToMeeting.windows[1].groups.length - 1;
-      while (self.GoToMeeting.windows[1].groups.length < 5) {
-        delay(1);
-      }
-      while (!self.GoToMeeting.windows[1].groups[4].checkboxes[0].enabled()) {
-        delay(1);
-      }
-    });
-
-    self.waitForFirstWindow = (function() {
-      console.log('waitForFirstWindow');
-      while (self.GoToMeeting.windows.length < 1) {
-        delay(1);
-      }
-    });
-
-    self.waitForSecondWindow = (function(callback) {
-      console.log('waitForSecondWindow');
-      while (self.GoToMeeting.windows.length < 2) {
-        delay(1);
-      }
-    });
-
-    return self;
-  });
-
-  var gotoMeeting = GoToMeetingStarter();
-  gotoMeeting.go();
-
-  return {};
-});
+var GoToMeeting = require('./start-gotomeeting');
 
 osa(GoToMeeting, {}, function(error) {
   console.log('osa complete', error);
--- start-gotomeeting.js
+++ start-gotomeeting.js
@@ -1,56 +1,58 @@
-module.exports = function(){
-
-  var GoToMeetingStarter = function(){
+module.exports = (function() {
+  var GoToMeetingStarter = (function() {
     var self = this;
 
-    self.go = function(){
+    self.go = (function() {
       var GoToMeetingApp = Application('/Applications/GoToMeeting.app');
       GoToMeetingApp.activate();
 
       var SystemEvents = Application('System Events');
       self.GoToMeeting = SystemEvents.processes['GoToMeeting'];
 
-      // Click Start Meeting
       self.waitForFirstWindow();
       GoToMeeting.windows[0].groups[3].buttons[0].click();
 
       self.waitForSecondWindow();
       self.waitForRecordButton();
       self.startRecording();
-    };
+    });
 
-    self.startRecording = function(){
+    self.startRecording = (function() {
       console.log('startRecording');
       var index = self.GoToMeeting.windows[1].groups.length - 1;
       GoToMeeting.windows[1].groups[index].checkboxes[0].click();
-    };
+    });
 
-    self.waitForRecordButton = function(){
+    self.waitForRecordButton = (function() {
       console.log('waitForRecordButton');
       var index = self.GoToMeeting.windows[1].groups.length - 1;
-      while(self.GoToMeeting.windows[1].groups.length < 5) {
+      while (self.GoToMeeting.windows[1].groups.length < 5) {
         delay(1);
       }
-      while(!self.GoToMeeting.windows[1].groups[4].checkboxes[0].enabled()){
+      while (!self.GoToMeeting.windows[1].groups[4].checkboxes[0].enabled()) {
         delay(1);
       }
-    };
+    });
 
-    self.waitForFirstWindow = function(){
+    self.waitForFirstWindow = (function() {
       console.log('waitForFirstWindow');
-      while(self.GoToMeeting.windows.length < 1) {
+      while (self.GoToMeeting.windows.length < 1) {
         delay(1);
       }
-    };
+    });
 
-    self.waitForSecondWindow = function(callback){
+    self.waitForSecondWindow = (function(callback) {
       console.log('waitForSecondWindow');
-      while(self.GoToMeeting.windows.length < 2) {
+      while (self.GoToMeeting.windows.length < 2) {
         delay(1);
       }
-    };
+    });
 
     return self;
-  };
+  });
 
-}
+  var gotoMeeting = GoToMeetingStarter();
+  gotoMeeting.go();
+
+  return {};
+});
