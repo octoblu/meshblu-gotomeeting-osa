156b8b5a83c655d5e5c273dcd6fe5d4da632ea36
Peter DeMartini <thepeterdemartini@gmail.com>
2015-09-23 09:15:22 -0700

    v1.0.5 updated connector

--- command.js
+++ command.js
@@ -1,62 +1,24 @@
 'use strict';
-var Plugin = require('./index').Plugin;
-var meshblu = require('meshblu');
-var config = require('./meshblu.json');
-var debug  = require('debug')('meshblu-gotomeeting-osa:index');
+var Connector     = require('./connector');
+var _             = require('lodash');
+var MeshbluConfig = require('meshblu-config');
+var meshbluConfig = new MeshbluConfig({});
 
-var conx = meshblu.createConnection({
-  server : config.server,
-  port   : config.port,
-  uuid   : config.uuid,
-  token  : config.token
-});
+var connector = new Connector(meshbluConfig.toJSON());
 
-conx.on('notReady', function(){
-  debug('notReady');
-  console.error.apply(this, arguments);
-});
-
-conx.on('error', function(){
-  debug('error');
-  console.error.apply(this, arguments);
-});
-
-var plugin = new Plugin();
-
-conx.on('ready', function(){
-  debug('ready');
-  conx.whoami({uuid: config.uuid}, function(device){
-    plugin.setOptions(device.options || {});
-    conx.update({
-      uuid: config.uuid,
-      token: config.token,
-      messageSchema: plugin.messageSchema,
-      optionsSchema: plugin.optionsSchema,
-      options:       plugin.options
-    });
-  });
-});
-
-conx.on('message', function(){
-  try {
-    plugin.onMessage.apply(plugin, arguments);
-  } catch (error){
-    console.error(error.message);
+connector.on('error', function(error) {
+  if(!error){
+    console.error('an unknown error occured');
+    return;
+  }
+  if(_.isPlainObject(error)){
+    console.error(error);
+    return;
+  }
+  console.error(error.toString());
+  if(error.stack){
     console.error(error.stack);
   }
 });
 
-conx.on('config', function(){
-  try {
-    plugin.onConfig.apply(plugin, arguments);
-  } catch (error){
-    console.error(error.message);
-    console.error(error.stack);
-  }
-});
-
-plugin.on('message', function(message){
-  conx.message(message);
-});
-
-plugin.on('error', console.error);
+connector.run();
